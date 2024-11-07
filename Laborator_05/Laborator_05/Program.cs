cf7de30c91cf88745ccf4b566f2e138a1a26a178
MirabelaVieriu <mirabelavieriu0@gmail.com>
2024-11-07 20:26:43 +0200

    Laboratoare 4 si 5

---
+++ Laborator_05/Laborator_05/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// Vieriu Maria Mirabela
+//grupa: 3131a
+
+
+namespace Laborator_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Window3D ex = new Window3D();
+            ex.Run(30.0, 0.0);
+        }
+    }
+}
